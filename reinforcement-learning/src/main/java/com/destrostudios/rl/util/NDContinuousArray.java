fd0515e785a0daf63d791d4a0be0920188184682
carlkuesters <carl.kuesters@youengineering.com>
2022-11-25 03:57:21 +0100

    Simplify interfaces, WIP.

--- reinforcement-learning/src/main/java/com/destrostudios/rl/util/NDContinuousArray.java
+++ reinforcement-learning/src/main/java/com/destrostudios/rl/util/NDContinuousArray.java
@@ -27,12 +27,6 @@ public class NDContinuousArray {
         } else {
             queue.remove();
             queue.offer(array);
-            NDArray[] buf = new NDArray[4];
-            int i = 0;
-            for (NDArray nd : queue) {
-                buf[i++] = nd;
-            }
-            return new NDList(NDArrays.stack(new NDList(buf[0], buf[1], buf[2], buf[3]), 1));
         }
         return new NDList(NDArrays.stack(new NDList(queue), 1));
     }
